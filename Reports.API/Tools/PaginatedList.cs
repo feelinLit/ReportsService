3b1cd575b07621e6cf3388de5be21dd5cb486248
De-Bill <podshivalov.o29@gmail.com>
2022-05-05 11:52:06 +0300

    feat: add properties to paginated list

--- Reports.API/Tools/PaginatedList.cs
+++ Reports.API/Tools/PaginatedList.cs
@@ -3,11 +3,13 @@
 public class PaginatedList<T> : List<T>
 {
     public int PageIndex { get; private set; }
+    public int PageSize { get; private set; }
     public int TotalPages { get; private set; }
 
     public PaginatedList(IReadOnlyCollection<T> source, int pageIndex, int pageSize)
     {
         PageIndex = pageIndex;
+        PageSize = pageSize;
         TotalPages = (int)Math.Ceiling(source.Count / (double)pageSize);
 
         var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
