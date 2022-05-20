edade5317ce1e5108000663af93e519e79ce2d04
De-Bill <podshivalov.o29@gmail.com>
2022-05-20 21:57:40 +0300

    feat: add some extra data to DbInitializer

--- Reports.DAL/Data/DbInitializer.cs
+++ Reports.DAL/Data/DbInitializer.cs
@@ -8,11 +8,15 @@ public static class DbInitializer
     {
         if (context.Employees.Any()) return;
 
-        var teamlead = new TeamLead("Lolek", null);
-        var employee2 = new Employee("Kolek", teamlead);
-        var employee3 = new Employee("Jojek", teamlead);
+        var teamlead = new TeamLead("UncleBob", null);
+        var employee2 = new Employee("Bobuk", teamlead);
+        var employee3 = new Employee("EricEvans", teamlead);
         var employee4 = new Employee("Xi", employee2);
-        context.Employees.AddRange(teamlead, employee2, employee3, employee4);
+        var employee5 = new Employee("Pudge", employee2);
+        var employee6 = new Employee("JoeBiden", employee2);
+        var employee7 = new Employee("ThomasCormen", employee6);
+        var employee8 = new Employee("DeBill", employee6);
+        context.Employees.AddRange(teamlead, employee2, employee3, employee4, employee5, employee6, employee7, employee8);
 
         var problem1 = employee2.AddProblem("Do Java Lab3");
         var problem2 = employee2.AddProblem("Rewrite Reports");
@@ -24,10 +28,12 @@ public static class DbInitializer
 
         employee4.AddComment(problem3, "Preparing ;)");
 
-        var report = employee2.AddReport("My first report");
-        report.AddProblem(problem1);
-        report.IsCompleted = true;
+        var reportCompleted = employee2.AddReport("My first report");
+        reportCompleted.AddProblem(problem1);
+        reportCompleted.IsCompleted = true;
 
+        var reportIncomplete = teamlead.AddReport("Report on completed team aims\nEveryone did a great job!");
+        
         context.SaveChanges();
     }
 }
